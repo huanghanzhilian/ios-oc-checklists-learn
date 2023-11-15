dc42298da159e86c46dda003711617e0fa6a5ec8
huanghanzhilian <1319639755@qq.com>
2023-11-15 14:58:15 +0800

    feat: use UIViewController

--- Checklists/ViewController.m
+++ Checklists/ViewController.m
@@ -18,5 +18,16 @@
     // Do any additional setup after loading the view.
 }
 
+//(NSInteger)     tableView:   (UITableView *) tableView  numberOfRowsInSection:   (NSInteger)  section
+// 返回值类型    方法名称的一部分     第一个参数的类型  参数的变量  方法名称的一部分          第二个参数的类型   参数的变量
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+ return 15;
+}
+-(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath
+*)indexPath{
+
+ UITableViewCell *cell =[tableView dequeueReusableCellWithIdentifier:@"ChecklistItem"];
+ return cell;
+} 
 
 @end
